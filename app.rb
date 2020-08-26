2ad382a6675012f445810f630fdf2c2bceda7ac2
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-08-26 06:41:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  get '/food_form' do
+    erb :food_form
+  end
+  
   post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_room]}."
   end
 
 end
